7abec2f1841bfa53e9917d396a666de549748651
kovoo11 <62976501+kovoo11@users.noreply.github.com>
2020-04-01 19:45:42 +0800

    my answer to exercise1

--- exercise1.py
+++ exercise1.py
@@ -13,10 +13,21 @@ data = {
     'David': 'Biology,Chinese'
 }
 
-
+"""
 def reverse_dict(input_data):
     # TODO
     return output  # TODO
 
 
 reverse_dict(input_data=data)
+"""
+
+dict1 = {}
+for k, v in data.items():
+    for subjects in v.split(','):
+        if subjects not in dict1:
+            dict1[subjects] = [k]
+        else:
+            dict1[subjects].append(k)
+
+print(dict1)
